ad6fe94021f852b1f134028c2cd181d4a86b27e2
Rishi Nath <rishi.nath@gmail.com>
2020-03-12 10:45:29 -0700

    refactor pt 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import AnagramContainer from './game/AnagramContainer';
+import AnagramContainer from './anagram/AnagramContainer';
 
 export default function App() {
     return (
