eed15f9a7d99fbdb9865e4f8b079219d04870505
Rishi Nath <rishi.nath@gmail.com>
2020-03-15 11:16:38 -0700

    example game data structure

--- server/index.js
+++ server/index.js
@@ -15,24 +15,62 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const client = new MongoClient(connection_string, { useNewUrlParser: true });
+const cluster = new MongoClient(connection_string, { useNewUrlParser: true });
 
-client.connect(err => {
+let collection;
+
+cluster.connect(err => {
     assert.equal(null, err);
     console.log("Connected successfully to cluster!");
 
-    const collection = client.db("anagram-game").collection("games");
+    collection = cluster.db("anagram-game").collection("games");
 
-    collection.insertOne({
-        test: "this is a test"
-    }).then(err => {
-        client.close();
-    });
+    initServer();
 });
 
-// io.on('connection', socket => {
-//     console.log('A socket connected!');
-// });
+function initServer() {
+    io.on('connection', socket => {
+        console.log('A socket connected!');
+    });
 
-// server.listen(port, _ => console.log("Listening on port ", port));
+    server.listen(port, _ => console.log("Listening on port ", port));
+}
+
+const example_game = {
+    uuid: 'fwu0e8gtabofubeg8g',
+    states: {
+        user_id_1: {
+            words: ['dog, cat, mouse'],
+            stage: 'finished',
+            score: 200,
+        },
+        user_id_2: {
+            words: ['mouse'],
+            stage: 'finished',
+            score: 70,
+        },
+    },
+    config: {
+        letters: ['DOGCATMOUSE'],
+        duration: 30,
+    }
+};
+
+function updateGame(uuid, user_id, game_obj) {
+    collection.updateOne(
+        { uuid: uuid },
+        { $set: { ['states.' + user_id]: game_obj.state } }
+    );
+}
+
+function getGame(uuid) {
+    return collection.findOne(
+        { uuid: uuid }
+    );
+}
+
+
+
+
+
 
