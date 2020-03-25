f2b6b53fbaa3723a375793a5dfe36831743cde0b
Rishi Nath <rishi.nath@gmail.com>
2020-03-25 14:46:47 -0700

    user logic into server, more ts

---
+++ frontend/types/index.d.ts
@@ -0,0 +1,28 @@
+export type AnagramStage = 'NOT-STARTED' | 'RUNNING' | 'FINISHED';
+
+export interface AnagramState {
+    stage: AnagramStage,
+    words: string[],
+    score: number
+}
+
+export interface AnagramConfig {
+    letters: string[],
+    duration: number
+}
+
+export interface AnagramObject {
+    uuid: string,
+    states: {
+        [user_id: string]: AnagramState
+    },
+    users: User[],
+    config: AnagramConfig
+}
+
+export interface User {
+    user_id: string,
+    username: string
+}
+
+
