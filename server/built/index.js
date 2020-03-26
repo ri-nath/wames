dee8d9e478fd1691146496f5c59a7525f6226e64
Rishi Nath <rishi.nath@gmail.com>
2020-03-25 23:22:41 -0700

    commit build files

---
+++ server/built/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Server_1 = require("./Server");
+const server = new Server_1.default();
+//# sourceMappingURL=index.js.map
